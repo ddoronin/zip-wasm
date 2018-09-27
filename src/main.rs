48bdbe1a4409105847973fa8fe3c2e07b301ae05
Dima Doronin <doronindm@gmail.com>
2018-09-27 01:18:45 -0400

    integration

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,7 @@ use std::io::Write;
 use std::mem;
 use std::slice;
 use std::os::raw::c_void;
-use libflate::gzip::Decoder;
-use libflate::gzip::Encoder;
+use libflate::gzip::{Encoder, EncodeOptions, HeaderBuilder, Decoder};
 
 fn main() {}
 
@@ -30,15 +29,16 @@ pub extern "C" fn gzCompress(pointer: *mut u8, byte_size: usize, meta: *mut usiz
     let data = unsafe { slice::from_raw_parts_mut(pointer, byte_size) };
     let meta_data = unsafe { slice::from_raw_parts_mut(meta, 1) };
 
-    let mut buf = Vec::new();
-    Encoder::new(&mut buf)
-        .unwrap()
-        .write_all(&data[..])
-        .unwrap();
+    let header = HeaderBuilder::new().modification_time(123).finish();
+    let options = EncodeOptions::new().no_compression().header(header);
+    
+    let mut encoder = Encoder::with_options(Vec::new(), options).unwrap();
+    encoder.write_all(&data).unwrap();
+    let mut res = encoder.finish().into_result().unwrap();
 
-    meta_data[0] = buf.len();
-    let ptr = buf.as_mut_ptr();
-    mem::forget(buf);
+    meta_data[0] = res.len();
+    let ptr = res.as_mut_ptr();
+    mem::forget(res);
     ptr as *mut c_void
 }
 
